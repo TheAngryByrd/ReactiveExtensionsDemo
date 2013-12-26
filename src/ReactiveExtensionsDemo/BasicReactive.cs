54d5185c77894d700b010e7ce950955340b7a070
Jimmy Byrd <jimmybyrd@gmail.com>
2013-12-26 13:35:38 -0500

    Added Observable.Range

---
+++ src/ReactiveExtensionsDemo/BasicReactive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reactive.Linq;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ReactiveExtensionsDemo
+{
+    [TestClass]
+    public class BasicReactive
+    {
+        [TestMethod]
+        public void ObservableRange()
+        {
+            var range = Observable.Range(0, 10);
+
+            range.Subscribe(x => Debug.WriteLine(x),
+                            e => Debug.WriteLine(e),
+                            () => Debug.WriteLine("finished"));
+        }   
+        
+        
+
+
+
+    }
+}
